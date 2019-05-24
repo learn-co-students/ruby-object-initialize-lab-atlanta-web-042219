e81cb37e66ddb2df6964568db45497dddc8e95d0
github username <github email address>
2019-05-24 11:29:14 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,28 +1,10 @@
 class Dog
+    attr_accessor :name, :breed
 
-def initialize(name, breed = "Mutt")
-    @name = name
-    @breed = breed
-end
-
-  def name=(name, breed = "Mutt")
-    @name = name
-    @breed = breed
-  end
-
-  def name
-    @name
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
-
-end
+    def initialize(name, breed = "Mutt")
+        @name = name 
+        @breed = breed
+    end 
 
 
-fido = Dog.new("Fido")
+end 
